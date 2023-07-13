526e21d553f1c93397a85eae05e96dc4864a1056
SO4REN <ale.catinello@gmail.com>
2023-07-13 22:15:35 +0200

    feat: weekly cache file

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from src.dataParser import *
 
+from datetime import datetime
 from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, OperatingSystem
+
+
+
+def createTitleList(path, url, userAgentRotator):
+    movies = []
+    movies.append(datetime.today().strftime('%Y-%m-%d'))
+    movies.extend(getCatalogueTitles(url, userAgentRotator))
+
+    with open(path, "w") as f:
+        f.write("\n".join(movies))
+    
+    return movies[1:]
 
 
 def main():
@@ -9,11 +22,24 @@ def main():
     operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.MAC.value]
     userAgentRotator = UserAgent(software_names=software_names, operating_systems=operating_systems)
     
-    for elem in ["film", "serietv"]:
+    for elem in ["serietv", "film"]:
         url = "https://movieplayer.it/" + elem + "/streaming/netflix/"
-        dfPath = os.path.join(os.path.join(os.getcwd(), "data", "{elem}.parquet".format(elem=elem)))
+        path = os.path.join(os.path.join(os.getcwd(), "data", "{elem}".format(elem=elem)))
+        
+        titleList = path + ".txt"
+        dfPath = path + ".parquet"
 
-        movies = getCatalogueTitles(url, userAgentRotator)
+        movies = []
+        if os.path.exists(titleList):
+            with open(titleList, "r") as f:
+                movies = f.readlines()
+            date = movies[0].strip()
+            if (datetime.today() - datetime.strptime(date, '%Y-%m-%d')).days < 7:
+                movies = movies[1:]
+            else:
+                movies = createTitleList(titleList, url, userAgentRotator)
+        else:
+            movies = createTitleList(titleList, url, userAgentRotator)
         print("Trovati {n} {type}".format(n=len(movies), type=elem))
 
         dfMovies = getData(movies, userAgentRotator, dfPath)
