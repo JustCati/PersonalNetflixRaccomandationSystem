c1e6dc064a5cccd29afd58ba6bfa75d9d36feba5
SO4REN <ale.catinello@gmail.com>
2023-07-21 17:42:51 +0200

    fix: dtypes on numpy default initialization

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import numpy as np
 import pandas as pd
 
@@ -11,22 +10,13 @@ def main():
     if os.path.exists("dataset.parquet"):
         df = pd.read_parquet("dataset.parquet")
     else:
-        df = getDataset()
-        df["Embedding"] = [np.full((1024,), np.inf) for _ in range(len(df))] #! <3
+        df = getDataset(update=False)
+        df["Embedding"] = [np.full((1024,), np.inf, dtype=np.float32) for _ in range(len(df))] #! <3
         df.to_parquet("dataset.parquet")
 
-    #*-------------------------------------
-    API_KEY = None
-    if len(sys.argv) > 1:
-        API_KEY = sys.argv[1]
-        if ".env" in API_KEY:
-            with open(API_KEY) as f:
-                API_KEY = f.read().strip()
-        API_KEY = API_KEY.split("=")[1]
-
-    
+    #*------------------------------------- 
     getEmbeddings_E5_LargeV2(df, shuffle=True)
-    # getEmbeddingsOpenAI(df, API_KEY, shuffle=True)
+    # getEmbeddingsOpenAI(df, shuffle=True)
     #*-------------------------------------
 
 
