35f503c41f16f146136ad3045601fc7bc1e5f654
SO4REN <ale.catinello@gmail.com>
2023-07-23 23:09:46 +0200

    feat: euclidean distance similarity and dot product WIP: SVM similariti

--- distances.py
+++ distances.py
@@ -1,8 +1,24 @@
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
+
+from sklearn.svm import SVC, LinearSVC
+from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 
 
-def getSimilarityMatrix(df, colName="Embeddings_Trama"):
+
+
+def getSimilarityMatrix(df, colName="Embeddings_Trama", method="cosine"):
     embeddings = df[colName].values
     embeddings = np.vstack(embeddings)
-    return cosine_similarity(embeddings)
+    return cosine_similarity(embeddings) if method == "cosine" else euclidean_distances(embeddings)
+
+
+# def getSimilaritiesSVM(df, colName="Embeddings_Trama"):
+#     x = np.vstack(df[colName].values)
+#     y = np.zeros(len(df))
+#     y[0] = 1
+    
+#     clf = LinearSVC(class_weight='balanced', verbose=False, max_iter=100000, tol=1e-6, C=0.1, dual="auto")
+#     clf.fit(x, y)
+    
+#     similarities = clf.decision_function(x)
+#     return similarities
--- main.py
+++ main.py
@@ -35,24 +35,52 @@ def main():
     df = getFeatureAttori(df, colName="Regia")
     #*-----------------------------------------------------------------------
 
-    # The Conjuring - Il caso Enfield
-    # x["Embeddings_Attori"], x["Embeddings_Tipologia"], x["Embeddings_Regia"]
-    #*---------- Get Similarity Matrix ------------
+    #*---------- Get Rapresentation Vector ------------
     df["allEmbeddings"] = df.apply(lambda x: np.concatenate((x["Embeddings_Trama"], x["Embeddings_Genere"], x["Embeddings_Regia"], x["Embeddings_Attori"], x["Embeddings_Tipologia"]), dtype=np.float32), axis=1)
     df.allEmbeddings = df.apply(lambda x: StandardScaler().fit_transform(x["allEmbeddings"].reshape(-1, 1)).reshape(-1), axis=1)
-    
-    similarity_matrix = getSimilarityMatrix(df, colName="allEmbeddings")
     #*--------------------------------------------
     
+        
+    #*---------- Naive --------
+    while((title := input("Inserisci il titolo del film: ").strip()) not in df.Titolo.values):
+            print("Film non trovato")
+    index = df[df.Titolo == title].index[0]
     
-    #*---------- Naive ------------
-    while((movie := input("Inserisci il titolo del film: ").strip()) not in df.Titolo.values):
-        print("Film non trovato")
+    for distance in ("cosine", "euclidean"):
+        similarity_matrix = getSimilarityMatrix(df, colName="allEmbeddings", method=distance)
+        
+        print(f"Film simili con {distance} come distanza:")
+        if distance == "cosine":
+            print(df.iloc[np.argsort(similarity_matrix[index])[-11::]]["Titolo"][-2::-1])
+        elif distance == "euclidean":
+            print(df.iloc[np.argsort(similarity_matrix[index])[10::-1]]["Titolo"][-2::-1])
 
-    index = df[df.Titolo == movie].index[0]
-    print("Film simili:")
-    print(df.iloc[np.argsort(similarity_matrix[index])[-11:]]["Titolo"][-2::-1])
-    #*-----------------------------
+    #! Dot Product (unused because embeddings is scaled so dot product is the same as cosine similarity)
+    # movie = df.allEmbeddings.values[index]
+    # embeddings = np.array([np.array(elem) for elem in df.allEmbeddings.values])
+    
+    # similar = embeddings.dot(movie)
+    # sorted_idx = np.argsort(-similar)
+    
+    # print(f"Film simili con dot product:")
+    # print(df.iloc[sorted_idx[10::-1]]["Titolo"][-2::-1])
+
+    #! Linear SVC
+    # movie = df.allEmbeddings.values[index]
+    # x = np.concatenate([movie[None, ...], [np.array(elem) for elem in df.allEmbeddings.values]])
+    # y = np.zeros(len(df) + 1)
+    # y[0] = 1
+    
+    # clf = LinearSVC(class_weight='balanced', verbose=False, max_iter=100000, tol=1e-6, C=0.1)
+    # clf.fit(x, y)
+    
+    # similarities = clf.decision_function(x)
+    # sorted_idx = np.argsort(-similarities)
+    
+    # print(f"Film simili con SVM:")
+    # print(df.iloc[sorted_idx[-11::]]["Titolo"][-2::-1])
+    #*-------------------------------
+    
 
 
 if __name__ == "__main__":
