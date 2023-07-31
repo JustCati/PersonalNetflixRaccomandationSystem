49d569097332e7980ead7216ddf2a1bae0f1637d
SO4REN <ale.catinello@gmail.com>
2023-07-31 18:07:59 +0200

    drop: include similarity matrix in distances

--- raccomend/distances.py
+++ raccomend/distances.py
@@ -8,3 +8,15 @@ def getSimilarityMatrix(df, colName="Embeddings_Trama", method="cosine"):
     embeddings = np.vstack(embeddings)
     return cosine_similarity(embeddings) if method == "cosine" else euclidean_distances(embeddings)
 
+
+def getMostSimilarCosine(movies, embeddings, title):
+    index = movies[movies.Titolo == title].index[0]
+    similarity_matrix = getSimilarityMatrix(embeddings, colName="allEmbeddings", method="cosine")
+    return movies.iloc[np.argsort(similarity_matrix[index])[-11::]]["Titolo"][-2::-1]
+    
+        
+        
+def getMostSimilarEuclidean(movies, embeddings, title):
+    index = movies[movies.Titolo == title].index[0]
+    similarity_matrix = getSimilarityMatrix(embeddings, colName="allEmbeddings", method="euclidean")
+    return movies.iloc[np.argsort(similarity_matrix[index])[10::-1]]["Titolo"][-2::-1]
