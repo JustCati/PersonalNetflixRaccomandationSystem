0bf551999559a4dc1aa096f89ee01d8c69179e42
SO4REN <ale.catinello@gmail.com>
2023-08-16 19:51:21 +0200

    style: remove whitespace

--- raccomend/distances.py
+++ raccomend/distances.py
@@ -13,9 +13,8 @@ def getMostSimilarCosine(movies, embeddings, title):
     index = movies[movies.Titolo == title].index[0]
     similarity_matrix = getSimilarityMatrix(embeddings, colName="allEmbeddings", method="cosine")
     return movies.iloc[np.argsort(similarity_matrix[index])[-11::]]["Titolo"][-2::-1]
-    
-        
-        
+
+
 def getMostSimilarEuclidean(movies, embeddings, title):
     index = movies[movies.Titolo == title].index[0]
     similarity_matrix = getSimilarityMatrix(embeddings, colName="allEmbeddings", method="euclidean")
