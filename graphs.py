6c3aab4303eb1f205f89284b8a84e51f066ba086
SO4REN <ale.catinello@gmail.com>
2023-09-06 18:43:57 +0200

    feat: add graph for best K

---
+++ graphs.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def plotBestK():
+    with open("bestK.txt", "r") as f:
+        lines = f.readlines()
+
+        counts = []
+        bestKs = []
+        for line in lines:
+            count, bestK = line.split(", ")
+            counts.append(int(count))
+            bestKs.append(int(bestK))
+
+        num = 0
+        for k, elem in zip(bestKs, counts):
+            if k == elem or k == elem - 1:
+                num += 1
+        perc = num / len(bestKs) * 100
+
+        plt.plot([0, 50], [0, 50], color="red", linestyle="--")
+        plt.plot(counts, bestKs, linestyle="solid", marker="o", color="blue")
+        plt.xlabel("Numero di film nel Training Set")
+        plt.ylabel("Migliore K")
+        plt.grid(True)
+
+        plt.xlim(1, 52)
+        plt.ylim(1, 52)
+        plt.xticks(np.arange(1, 52, 1))
+        plt.yticks(np.arange(1, 52, 1))
+        plt.tick_params(axis='both', which='major', labelsize=8)
+
+        plt.text(5, 43, f"Percentuale di K corrispondente\nal massimo possibile (±1) per istanza: {perc:.0f}%", bbox=dict(facecolor='white', alpha=1))        
+        plt.show()
+
+
+if __name__ == "__main__":
+    # plotBestK()
+    exit()
